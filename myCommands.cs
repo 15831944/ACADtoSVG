f6222df6f5508e3f9432671b309f107c13989939
Gessal <32535685+Gessal@users.noreply.github.com>
2019-09-06 15:27:47 +0700

    Upload
    Need:
    1) color by layer bug fix
    2) optional SVG layer syze

---
+++ myCommands.cs
@@ -0,0 +1,23 @@
+﻿using Autodesk.AutoCAD.Runtime;
+using Autodesk.AutoCAD.ApplicationServices;
+using Autodesk.AutoCAD.DatabaseServices;
+using Autodesk.AutoCAD.EditorInput;
+using System.Xml;
+using Autodesk.AutoCAD.Geometry;
+using System;
+using System.IO;
+
+[assembly: CommandClass(typeof(AutoCAD_PointsReader.MyCommands))]
+
+namespace AutoCAD_PointsReader
+{
+    public class MyCommands
+    {
+        /*************************************************************************/
+        [CommandMethod("ConvertToSVG", CommandFlags.Modal | CommandFlags.UsePickSet)]
+        public void ConverttoSVG()
+        {
+            Utils.Utils.ConvertToSVG();
+        }
+    }
+}
